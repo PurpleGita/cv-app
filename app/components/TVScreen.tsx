733f9a786708a59780de2b571bf4af490bba11da
PurpleGita <rasmus.munk01@gmail.com>
2026-04-21 17:51:16 +0200

    added transparent polaroid

---
+++ app/components/TVScreen.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function TVScreen() {
+  return (
+    <div className="w-full h-full relative">
+      <div className="absolute top-[15%] absolute left-[30%] -translate-x-1/2">
+        <p className="opacity-80 text-[2vw] font-['PixelifySans']">Welcome to my CV</p>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,51 @@
+'use client';
+
+import React from 'react';
 import Image from "next/image";
+import CRTEffect from 'vault66-crt-effect';
+import "vault66-crt-effect/dist/vault66-crt-effect.css";
+import TVScreen from './components/TVScreen';
 
 export default function Home() {
   return (
     <div className="relative w-full h-dvh overflow-hidden flex items-center justify-center p-2">
-      {/* TV Image Container */}
-      <div className="relative w-full h-full max-w-15xl max-h-[100vh]">
+      <div
+        className="relative w-full max-w-15xl max-h-[100vh]"
+        style={{ aspectRatio: '1920 / 1012' }}
+      >
+        <div className="absolute inset-0 z-0 overflow-hidden">
+          <div className="absolute inset-[10%] min-h-0 min-w-0">
+            <CRTEffect 
+              enabled={true}
+              sweepDuration={5}
+              sweepThickness={20}
+              scanlineOpacity={0.1}
+              theme="blue"
+              enableScanlines={true}
+              scanlineThickness={4}
+              enableSweep={true}
+              enableGlow={false}
+              enableEdgeGlow={true}
+              edgeGlowColor="rgba(64, 10, 134, 0.9)"
+              edgeGlowSize={70}
+              enableFlicker={false}
+              enableGlitch={false}
+              glitchIntensity={0.4}
+              glitchSpeed={9}
+            >
+
+              <TVScreen />
+            </CRTEffect>
+          </div>
+        </div>
+
         <Image
-          src="/TvSketch.png"
+          src="/TvSketchAgain.png"
           alt="Background image yaaa"
           fill
-          className="object-contain"
+          className="object-contain z-10"
           priority
         />
-        
-        {/* Content on top of image */}
-        <div className="absolute inset-0 flex flex-col items-center justify-center">
-          <h1 className="text-white text-4xl font-bold">Your content here</h1>
-        </div>
       </div>
     </div>
   );
