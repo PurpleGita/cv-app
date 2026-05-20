b43f2ce03f09f344a0695f04e2ca6604b0897bbd
PurpleGita <rasmus.munk01@gmail.com>
2026-05-20 16:57:57 +0200

    updated image source to be case sensetive since render apparently is.

--- app/components/TVStatic.tsx
+++ app/components/TVStatic.tsx
@@ -5,7 +5,7 @@ export default function TVStatic() {
   return (
     <div className="w-full h-full relative absolute left-[-5%]">
                 <Image
-                  src="/TVStatic.jpg"
+                  src="/TvStatic.jpg"
                   alt="TV Static"
                   fill
                   className="object-contain z-10 pointer-events-none"
